4b15def03229edf6d2b1dd47a1d46adaff832a2d
20075835 <20075835@IT118PC26.wit.ie>
2017-10-20 14:40:29 +0100

    Added new tests for Activity and updated user test

--- test/models/UserTest.java
+++ test/models/UserTest.java
@@ -5,12 +5,18 @@ import static org.junit.Assert.*;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.junit.Test;
 
-import static models.Fixtures.users;
+import org.junit.Test;
 
 public class UserTest
 {
+  private User[] users =
+  {
+    new User ("marge", "simpson", "marge@simpson.com",  "secret"),
+    new User ("lisa",  "simpson", "lisa@simpson.com",   "secret"),
+    new User ("bart",  "simpson", "bart@simpson.com",   "secret"),
+    new User ("maggie","simpson", "maggie@simpson.com", "secret")
+  };
   User homer = new User ("homer", "simpson", "homer@simpson.com",  "secret");
 
   @Test
@@ -26,7 +32,7 @@ public class UserTest
   public void testIds()
   {
     Set<Long> ids = new HashSet<>();
-	for (User user : users)
+    for (User user : users)
     {
       ids.add(user.id);
     }
@@ -36,6 +42,6 @@ public class UserTest
   @Test
   public void testToString()
   {
-    assertEquals ("User{homer, simpson, secret, homer@simpson.com}", homer.toString());
+    assertEquals ("User{" + homer.id + ", homer, simpson, secret, homer@simpson.com}", homer.toString());
   }
 }
